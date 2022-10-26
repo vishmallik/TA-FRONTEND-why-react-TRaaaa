088cf9eb186cacfd516aa47b0d7cfedc65a2d209
visawjeet mallik <vishmallik@gmail.com>
2022-10-26 10:53:19 +0530

    separate Article from Articles

---
+++ block-BRaaao/news/src/components/Article.js
@@ -0,0 +1,28 @@
+function Article(props) {
+  return (
+    <div className="article">
+      <div className="img-parent">
+        <img src={props.urlToImage} alt={props.title} />
+      </div>
+      <div className="text-parent">
+        <h3>{props.title}</h3>
+        <hr />
+        <div className="author">
+          <p>
+            <i className="fas fa-user"></i>
+            {props.author}
+          </p>
+          <p>
+            <i className="fa-solid fa-clock"></i>
+            {props.publishedAt.slice(0, 10)}
+          </p>
+        </div>
+        <h4>{props.description}</h4>
+        <p>{props.content}</p>
+        <a href={props.url}>Read more...</a>
+      </div>
+    </div>
+  );
+}
+
+export default Article;
